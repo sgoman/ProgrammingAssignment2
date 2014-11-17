688cf25f515416ab5a5417308124b5c98af935df
IQ Gobo <iq.gobo@gmx.net>
2014-11-17 20:25:48 +0100

    reformatted code for easier readability
    Keep in mind though that I use tabs instead of spaces, so the actual indentation relies on your configuration (how many spaces used per tab, if using fixed width fonts etc).

--- cachematrix.R
+++ cachematrix.R
@@ -5,19 +5,32 @@
 makeCacheMatrix <- function(x = matrix()) {
   # c is the cache, defaults to NULL on initialization
   c <- NULL
+  
   # setter method resets x to the new matrix and c back to NULL
   set <- function(y) {
     x <<- y
     c <<- NULL
   }
+  
   # getter method to return the matrix value x
-  get <- function() x
+  get <- function() {
+    x
+  }
+  
   # setter method to store an inverted matrix in the cache for future reference
-  setInverse <- function(inv) c <<- inv
+  setInverse <- function(inv) {
+    c <<- inv
+  }
+  
   # getter method to return the cached inverse of the matrix (might be NULL!)
-  getInverse <- function() c
+  getInverse <- function() {
+    c
+  }
+  
   # set the return list to enumerate over this objects methods
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -26,18 +39,23 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## get the cached value of x
   i <- x$getInverse()
+  
   if (!is.null(i)) {
     ## the cache is not NULL, therefore just return its value
     message("getting cached data")
     return (i)
   }
+  
   ## if we reach this point the cache was NULL
   ## get the original matrix into data
   data <- x$get()
+  
   ## solve data to get the inverse matrix
   i <- solve(data)
+  
   ## store the inverse in the original object using its method
   x$setInverse(i)
+  
   ## return the newly computed inverse
   i
 }
